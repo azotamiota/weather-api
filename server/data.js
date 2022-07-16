0a697482e19b6234b06644fe6b8183c58838cc86
azotamiota <azotamiota@gmail.com>
2022-07-16 23:27:43 +0100

    create sample weather data

---
+++ server/data.js
@@ -0,0 +1,20 @@
+const weather = [
+    {London : {Celsius: 19,
+                Fahrenheit: 66,
+                wind: true,
+            rain: true}},
+    {Madrid : {Celsius: 28,
+                Fahrenheit: 82,
+                wind: false,
+            rain: false}}, 
+    {Rome : {Celsius: 27,
+                Fahrenheit: 80,
+                wind: true,
+            rain: false}},
+    {Oslo : {Celsius: 12,
+                Fahrenheit: 53,
+                wind: false,
+            rain: true}},   
+]
+
+module.exports = weather
