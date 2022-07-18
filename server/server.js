39ed4bfd9508e23284f6dfd1962a48d0f46251c5
azotamiota <azotamiota@gmail.com>
2022-07-18 11:08:20 +0100

    Add DELETE request

--- server/server.js
+++ server/server.js
@@ -14,7 +14,6 @@ app.get('/cities/:city', (req, res) => {
     const currentCity = weather.filter(c => {
         return Object.keys(c).includes(city)
     })
-    console.log(currentCity)
     if(currentCity.length > 0) {
         res.status(200).send(currentCity[0])
     } else {
@@ -26,7 +25,7 @@ app.post('/cities', (req, res) => {
     const newCity = req.body
     const newID = weather.length + 1
     weather.push({id: newID, ...newCity})
-    res.status(201).json({id: newID, ...newCity}) // .end(`${newCity} has been saved successfully with an id of ${newID} to the database`)
+    res.status(201).json({id: newID, ...newCity})
 })
 
 app.patch('/cities/:city', (req, res) => {
@@ -37,15 +36,26 @@ app.patch('/cities/:city', (req, res) => {
     weather = weather.filter(c => {
         return !Object.keys(c).includes(city)
     })
-    console.log('weather: ', weather)
     if(currentCity.length < 1) {
         res.status(404).send(`<h1>404 Not found <br> ${req.params.city} is not in the database</h1>`)
     } else {
-        // console.log('city: ', city, '\nObject.keys(req.body)[0]: ', Object.keys(req.body)[0], '\nObject.values(req.body)[0]: ', Object.values(req.body)[0])
         currentCity[0][city][Object.keys(req.body)[0]] = Object.values(req.body)[0]
-        // console.log('currentCity: ', currentCity)
         weather.push(currentCity[0])
         res.status(202).send(currentCity[0])
+    }
+})
+
+app.delete('/cities/:city', (req, res) => {
+    const city = req.params.city.toUpperCase()
+    const citiesInDB = weather.map(c => Object.keys(c)).map(entry => entry[1])
+    console.log(citiesInDB);
+    if(citiesInDB.includes(city)) {
+        weather = weather.filter(c => {
+            return !Object.keys(c).includes(city)
+        })
+        res.status(201).send(`${req.params.city} has been deleted`)
+    } else {
+        res.status(404).send(String(`${req.params.city} is not in database`))
     }
 })
 
