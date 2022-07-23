be9c71de6dabe2fc86e94dce9dada22542771074
azotamiota <azotamiota@gmail.com>
2022-07-23 15:27:02 +0100

    front end scratch ready

--- client/assets/index.js
+++ client/assets/index.js
@@ -3,14 +3,14 @@ const mainGrid = document.getElementById('main-grid')
 
 const fetchInitialData = () => {
 
-    // mainGrid.textContent = 'Loading...'
+    mainGrid.textContent = 'Loading...'
 
     fetch(root)
         .then(res => res.json())
         .then(data => {
             displayData(data)
         }).catch(err => {
-            // mainGrid.textContent = 'Error while fetching data, try again later.';
+            mainGrid.textContent = 'Error while fetching data, try again later.';
             console.log(err)
         })
 }
@@ -18,11 +18,7 @@ const fetchInitialData = () => {
 fetchInitialData()
 
 const displayData = (data) => {
-    
-    // createCity.setAttribute('class', 'grid-elem')
-    
-    const createAddons = document.createElement('h5')
-    // mainGrid.textContent = '';
+    mainGrid.textContent = '';
 
     for (let elem in data) {
         console.log('data[elem]: ', data[elem])
@@ -30,12 +26,16 @@ const displayData = (data) => {
         createGridElement.setAttribute('id', `grid-elem-${elem}`)
         mainGrid.appendChild(createGridElement)
         const gridElem = document.querySelector(`#grid-elem-${elem}`)
+        gridElem.setAttribute('class', 'grid-elem')
         const createCity = document.createElement('h3')
         gridElem.appendChild(createCity)
         createCity.textContent = data[elem]['city']
         const createTemp = document.createElement('h4')
-        gridElem.append(createTemp)
+        gridElem.appendChild(createTemp)
         createTemp.textContent = `Celsius: ${data[elem]['Celsius']}, Fahrenheit: ${data[elem]['Fahrenheit']}`
+        const createAddons = document.createElement('h5')
+        gridElem.appendChild(createAddons)
+        createAddons.textContent = (data[elem]['wind'] ? 'windy' : '') + (data[elem]['rain'] ? ' rainy' : '')
     }
 
 }
