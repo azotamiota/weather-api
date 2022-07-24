3353ee20caab650f64e37ad2b82d54bde95a52e3
azotamiota <azotamiota@gmail.com>
2022-07-24 22:34:39 +0100

    apart from delete bug, app is ready

--- server.js
+++ server.js
@@ -13,32 +13,46 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))  // uncomment when connected with frontend form
 
 app.get('/', (req, res) => {
+    console.log('req.query.city: ', req.query.city)
+    console.log('req.query.delete: ', req.query.delete)
     readFile('./weather.json', 'utf-8', (err, jsonString) => {
         if(err){
             res.status(500).json({error: 'Internal server error, try again later'})
         }
         try {
             const data = JSON.parse(jsonString)
-            res.status(200).send(data)
-        } catch (error) {
-            res.status(500).send('Error: ', error)
-        }
-    })
-})
 
-app.get('/cities/:city', (req, res) => {
-    const city = req.params.city.toUpperCase()
-
-    readFile('./weather.json', 'utf-8', (err, jsonString) => {
-        if(err){
-            res.status(500).json({error: 'Internal server error, try again later'})
-        }
-        try {
-            const data = JSON.parse(jsonString)
-            for (let i = 1; i <= Object.entries(data).length; i++) {
-                if (data[i]['city'] === city) {
-                    res.status(200).send(data[i])
+            if (req.query.delete) {
+                for (let i = 1; i <= Object.keys(data).length; i++) {
+                    if (data[i].city === req.query.delete.toUpperCase()) {
+                        delete data[i]
+                        writeFile('./weather.json', JSON.stringify({...data}, null, 2), (err, result) => {
+                            if(err) console.log('Error while deleting object property: ', err);
+                        res.status(301).redirect('http://127.0.0.1:5501/index.html')
+                    })
+                    }
                 }
+            } else
+
+            if (req.query.city) {
+                for (let i = 1; i <= Object.keys(data).length; i++) {
+                console.log('data[String(i)].city: ', data[String(i)].city, 'data[i].city: ', data[i].city )
+                if (data[i].city === req.query.city.toUpperCase()) {
+                console.log('data i city: ',data[i].city, 'req body city', req.query.city.toUpperCase() )
+                data[i].city = req.query.city.toUpperCase()
+                data[i].Celsius = req.query.Celsius
+                data[i].Fahrenheit = req.query.Fahrenheit
+                data[i].wind = Boolean(req.query.wind)
+                data[i].rain = Boolean(req.query.rain)
+                    writeFile('./weather.json', JSON.stringify({...data}, null, 2), (err, result) => {
+                        if(err) console.log('Error while updating JSON: ', err);
+                        // console.log('result: ', result)
+                        res.status(301).redirect('http://127.0.0.1:5501/index.html')
+                    })
+                }
+                }
+            } else {
+                res.status(200).send(data)
             }
 
         } catch (error) {
@@ -46,6 +60,27 @@ app.get('/cities/:city', (req, res) => {
         }
     })
 })
+
+// app.get('/cities/:city', (req, res) => {
+//     const city = req.params.city.toUpperCase()
+
+//     readFile('./weather.json', 'utf-8', (err, jsonString) => {
+//         if(err){
+//             res.status(500).json({error: 'Internal server error, try again later'})
+//         }
+//         try {
+//             const data = JSON.parse(jsonString)
+//             for (let i = 1; i <= Object.entries(data).length; i++) {
+//                 if (data[i]['city'] === city) {
+//                     res.status(200).send(data[i])
+//                 }
+//             }
+
+//         } catch (error) {
+//             res.status(500).send('Error: ', error)
+//         }
+//     })
+// })
 
 app.post('/', (req, res) => {
     
@@ -60,48 +95,29 @@ app.post('/', (req, res) => {
                 "Celsius": req.body.celsius, "Fahrenheit": req.body.fahrenheit,
                 "wind": Boolean(req.body.wind), "rain" : Boolean(req.body.rain)}}, null, 2), (err, result) => {
                     if(err) console.log('Error while updating JSON: ', err);
-                    console.log('result: ', result)
+                    // console.log('result: ', result)
                     res.status(201).redirect('http://127.0.0.1:5501/index.html')
             })
         } catch (error) {
             console.log('Error while trying to write data: ', error)
             res.status(500).send('Error while trying to write data: ')
         }
-
-        
     })
 })
 
-app.patch('/cities/:city', (req, res) => {
-    const city = req.params.city.toUpperCase()
-    const currentCity = weather.filter(c => {
-        return Object.keys(c).includes(city)
-    })
-    weather = weather.filter(c => {
-        return !Object.keys(c).includes(city)
-    })
-    if(currentCity.length < 1) {
-        res.status(404).send(`<h1>404 Not found <br> ${req.params.city} is not in the database</h1>`)
-    } else {
-        currentCity[0][city][Object.keys(req.body)[0]] = Object.values(req.body)[0]
-        weather.push(currentCity[0])
-        res.status(202).send(currentCity[0])
-    }
-})
+// app.delete('/cities/:city', (req, res) => {
+//     const city = req.params.city.toUpperCase()
+//     const citiesInDB = weather.map(c => Object.keys(c)).map(entry => entry[1])
 
-app.delete('/cities/:city', (req, res) => {
-    const city = req.params.city.toUpperCase()
-    const citiesInDB = weather.map(c => Object.keys(c)).map(entry => entry[1])
-
-    if(citiesInDB.includes(city)) {
-        weather = weather.filter(c => {
-            return !Object.keys(c).includes(city)
-        })
-        res.status(201).send(`${req.params.city} has been deleted`)
-    } else {
-        res.status(404).send(String(`${req.params.city} is not in database`))
-    }
-})
+//     if(citiesInDB.includes(city)) {
+//         weather = weather.filter(c => {
+//             return !Object.keys(c).includes(city)
+//         })
+//         res.status(201).send(`${req.params.city} has been deleted`)
+//     } else {
+//         res.status(404).send(String(`${req.params.city} is not in database`))
+//     }
+// })
 
 
 module.exports = app
